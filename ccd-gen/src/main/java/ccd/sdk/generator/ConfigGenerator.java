ef4bc82c9bad0cb6e0988016fd3e7999018a0efc
Alex McAusland <alex.mcausland@gmail.com>
2019-12-04 10:50:31 +0000

    Generate config folder

--- ccd-gen/src/main/java/ccd/sdk/generator/ConfigGenerator.java
+++ ccd-gen/src/main/java/ccd/sdk/generator/ConfigGenerator.java
@@ -7,13 +7,45 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import org.apache.commons.lang3.StringUtils;
 import org.reflections.ReflectionUtils;
+import org.reflections.Reflections;
+import org.reflections.util.ConfigurationBuilder;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class ConfigGenerator {
+    private final File outputfolder;
+    private final Reflections reflections;
+
+    public ConfigGenerator(Reflections reflections, File outputFolder) {
+        this.reflections = reflections;
+        this.outputfolder = outputFolder;
+    }
+
+    public void generate() {
+        File complexTypes = new File(outputfolder, "ComplexTypes");
+        complexTypes.mkdir();
+
+        Set<Class<?>> types = reflections.getTypesAnnotatedWith(ComplexType.class);
+        for (Class<?> type : types) {
+            Path path = Paths.get(complexTypes.getPath(), type.getSimpleName() + ".json");
+            try {
+                Files.writeString(path, toComplexType(type));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
     public static String toComplexType(Class c) {
         Map<String, Object> label = getField(c);
         label.put("ListElementCode", c.getSimpleName().toLowerCase() + "Label");
--- ccd-gen/src/test/java/ccd/sdk/ComplexTypeTest.java
+++ ccd-gen/src/test/java/ccd/sdk/ComplexTypeTest.java
@@ -5,7 +5,10 @@ import ccd.sdk.generator.Builder;
 import ccd.sdk.types.ComplexType;
 import ccd.sdk.generator.DisplayContext;
 import com.google.common.io.Resources;
+import org.apache.commons.io.FileUtils;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.reflections.Reflections;
 import org.skyscreamer.jsonassert.JSONAssert;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
@@ -15,6 +18,7 @@ import uk.gov.hmcts.reform.fpl.model.Solicitor;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 
 import javax.validation.constraints.NotNull;
+import java.io.File;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Set;
@@ -26,19 +30,22 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class ComplexTypeTest {
+    @Rule
+    public TemporaryFolder temp = new TemporaryFolder();
+
     @Test
     public void solicitor() throws Exception {
 
         Reflections reflections = new Reflections("uk.gov.hmcts");
-        Set<Class<?>> types = reflections.getTypesAnnotatedWith(ComplexType.class);
-        assertThat(types.size(), equalTo(1));
-        Class c = getClass().getClassLoader().loadClass("uk.gov.hmcts.reform.fpl.model.Solicitor");
+
+        ConfigGenerator g = new ConfigGenerator(reflections, temp.getRoot());
+        g.generate();
+
         String expected = Resources.toString(Resources.getResource("ccd-definition/ComplexTypes/Solicitor.json"), Charset.defaultCharset());
+        String actual = FileUtils.readFileToString(new File(temp.getRoot(), "ComplexTypes/Solicitor.json"), Charset.defaultCharset());
 
-        JSONAssert.assertEquals(expected, ConfigGenerator.toComplexType(c), false);
-
+        JSONAssert.assertEquals(expected, actual, false);
     }
-
 
     @Test
     public void foo() {
