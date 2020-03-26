9df2f8918bcca55171540f9bf34714107634e835
Alex McAusland <alex.mcausland@gmail.com>
2020-03-26 14:11:12 +0000

    Remove use of field()

--- ccd-sdk-types/src/main/java/uk/gov/hmcts/ccd/sdk/types/FieldCollection.java
+++ ccd-sdk-types/src/main/java/uk/gov/hmcts/ccd/sdk/types/FieldCollection.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.ccd.sdk.types;
 
 import de.cronn.reflection.util.TypedPropertyGetter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
@@ -15,12 +14,12 @@ import uk.gov.hmcts.ccd.sdk.types.Field.FieldBuilder;
 
 @Builder
 @Data
-public class FieldCollection<T, Parent> {
+public class FieldCollection<Type, Parent> {
 
   @ToString.Exclude
   private List<Field.FieldBuilder> fields;
   @ToString.Exclude
-  private List<FieldCollectionBuilder<T, Parent>> complexFields;
+  private List<FieldCollectionBuilder<Type, Parent>> complexFields;
   @ToString.Exclude
   private List<Field.FieldBuilder> explicitFields;
 
@@ -35,7 +34,7 @@ public class FieldCollection<T, Parent> {
 
   private String rootFieldname;
 
-  public static class FieldCollectionBuilder<T, Parent> {
+  public static class FieldCollectionBuilder<Type, Parent> {
 
     Class dataClass;
 
@@ -48,11 +47,12 @@ public class FieldCollection<T, Parent> {
     private FieldCollectionBuilder<Parent, ?> parent;
     private PropertyUtils propertyUtils;
     private EventBuilder event;
-    private TypedPropertyGetter<T, ?> getter;
+    private TypedPropertyGetter<Type, ?> getter;
 
-    public static <T, Parent> FieldCollectionBuilder<T, Parent> builder(EventBuilder event,
-        FieldCollectionBuilder<Parent, ?> parent, Class<T> dataClass, PropertyUtils propertyUtils) {
-      FieldCollectionBuilder<T, Parent> result = new FieldCollectionBuilder<T, Parent>();
+    public static <Type, Parent> FieldCollectionBuilder<Type, Parent> builder(EventBuilder event,
+        FieldCollectionBuilder<Parent, ?> parent, Class<Type> dataClass,
+        PropertyUtils propertyUtils) {
+      FieldCollectionBuilder<Type, Parent> result = new FieldCollectionBuilder<Type, Parent>();
       result.pageId = "1";
       result.event = event;
       result.parent = parent;
@@ -67,67 +67,67 @@ public class FieldCollection<T, Parent> {
       return result;
     }
 
-    public FieldCollectionBuilder<T, Parent> optional(TypedPropertyGetter<T, ?> getter,
+    public FieldCollectionBuilder<Type, Parent> optional(TypedPropertyGetter<Type, ?> getter,
         String showCondition) {
       return field(getter, DisplayContext.Optional, showCondition);
     }
 
-    public FieldCollectionBuilder<T, Parent> optional(TypedPropertyGetter<T, ?> getter) {
+    public FieldCollectionBuilder<Type, Parent> optional(TypedPropertyGetter<Type, ?> getter) {
       return field(getter, DisplayContext.Optional);
     }
 
-    public FieldCollectionBuilder<T, Parent> mandatory(TypedPropertyGetter<T, ?> getter,
+    public FieldCollectionBuilder<Type, Parent> mandatory(TypedPropertyGetter<Type, ?> getter,
         String showCondition) {
       return field(getter, DisplayContext.Mandatory, showCondition);
     }
 
-    public FieldCollectionBuilder<T, Parent> mandatory(TypedPropertyGetter<T, ?> getter) {
+    public FieldCollectionBuilder<Type, Parent> mandatory(TypedPropertyGetter<Type, ?> getter) {
       return field(getter, DisplayContext.Mandatory);
     }
 
-    public FieldCollectionBuilder<T, Parent> readonly(TypedPropertyGetter<T, ?> getter,
+    public FieldCollectionBuilder<Type, Parent> readonly(TypedPropertyGetter<Type, ?> getter,
         String showCondition) {
       return field(getter, DisplayContext.ReadOnly, showCondition);
     }
 
-    public FieldCollectionBuilder<T, Parent> readonly(TypedPropertyGetter<T, ?> getter) {
+    public FieldCollectionBuilder<Type, Parent> readonly(TypedPropertyGetter<Type, ?> getter) {
       return field(getter, DisplayContext.ReadOnly);
     }
 
-    public FieldCollectionBuilder<T, Parent> field(String id, DisplayContext context,
+    public FieldCollectionBuilder<Type, Parent> field(String id, DisplayContext context,
         String showCondition, String type, String typeParam, String label) {
       explicitFields.add(field().id(id).context(context).showCondition(showCondition).type(type)
           .fieldTypeParameter(typeParam).label(label));
       return this;
     }
 
-    public FieldCollectionBuilder<T, Parent> field(String id, DisplayContext context,
+    public FieldCollectionBuilder<Type, Parent> field(String id, DisplayContext context,
         String showCondition) {
       explicitFields.add(field().id(id).context(context).showCondition(showCondition));
       return this;
     }
 
-    public FieldCollectionBuilder<T, Parent> field(String fieldName, DisplayContext context) {
+    public FieldCollectionBuilder<Type, Parent> field(String fieldName, DisplayContext context) {
       explicitFields.add(field().id(fieldName).context(context));
       return this;
     }
 
-    public FieldBuilder<T, Parent> field(String id) {
-      FieldBuilder<T, Parent> result = field().id(id);
+    public FieldBuilder<Type, Parent> field(String id) {
+      FieldBuilder<Type, Parent> result = field().id(id);
       explicitFields.add(result);
       return result;
     }
 
-    public <U> FieldBuilder<T, U> field(TypedPropertyGetter<T, U> getter) {
-      return (FieldBuilder<T, U>) field().id(getter);
+    public <U> FieldBuilder<Type, U> field(TypedPropertyGetter<Type, U> getter) {
+      return (FieldBuilder<Type, U>) field().id(getter);
     }
 
-    public FieldCollectionBuilder<T, Parent> field(TypedPropertyGetter<T, ?> getter,
+    public FieldCollectionBuilder<Type, Parent> field(TypedPropertyGetter<Type, ?> getter,
         DisplayContext context) {
       return field(getter, context, false);
     }
 
-    public FieldCollectionBuilder<T, Parent> field(TypedPropertyGetter<T, ?> getter,
+    public FieldCollectionBuilder<Type, Parent> field(TypedPropertyGetter<Type, ?> getter,
         DisplayContext context, String showCondition) {
       if (null != showCondition && null != rootFieldname) {
         showCondition = showCondition.replace("{{FIELD_NAME}}", rootFieldname);
@@ -136,14 +136,15 @@ public class FieldCollection<T, Parent> {
       return this;
     }
 
-    public FieldCollectionBuilder<T, Parent> field(TypedPropertyGetter<T, ?> getter,
+    public FieldCollectionBuilder<Type, Parent> field(TypedPropertyGetter<Type, ?> getter,
         DisplayContext context, boolean showSummary) {
       field().id(getter).context(context).showSummary(showSummary);
       return this;
     }
 
-    public Field.FieldBuilder<T, Parent> field() {
-      Field.FieldBuilder<T, Parent> f = Field.FieldBuilder.builder(dataClass, this, propertyUtils);
+    Field.FieldBuilder<Type, Parent> field() {
+      Field.FieldBuilder<Type, Parent> f = Field.FieldBuilder.builder(dataClass, this,
+          propertyUtils);
       f.page(this.pageId);
       fields.add(f);
       f.fieldDisplayOrder(++fieldDisplayOrder);
@@ -157,42 +158,42 @@ public class FieldCollection<T, Parent> {
       return parent;
     }
 
-    public FieldCollectionBuilder<T, Parent> showCondition(String condition) {
+    public FieldCollectionBuilder<Type, Parent> showCondition(String condition) {
       pageShowConditions.put(this.pageId.toString(), condition);
       return this;
     }
 
-    public FieldCollectionBuilder<T, Parent> midEventWebhook(String eventId) {
+    public FieldCollectionBuilder<Type, Parent> midEventWebhook(String eventId) {
       event.customWebhookName = eventId;
       String url = event.getWebhookPathByConvention(Webhook.MidEvent);
       midEventWebhooks.put(this.pageId.toString(), url);
       return this;
     }
 
-    public FieldCollectionBuilder<T, Parent> midEventWebhook() {
+    public FieldCollectionBuilder<Type, Parent> midEventWebhook() {
       String url = event.getWebhookPathByConvention(Webhook.MidEvent);
       midEventWebhooks.put(this.pageId.toString(), url);
       return this;
     }
 
-    public <U> FieldCollectionBuilder<T, Parent> complex(TypedPropertyGetter<T, ?> getter,
+    public <U> FieldCollectionBuilder<Type, Parent> complex(TypedPropertyGetter<Type, ?> getter,
         Class<U> c, Consumer<FieldCollectionBuilder<U, ?>> renderer, boolean showSummary) {
       renderer.accept(complex(getter, c, showSummary));
       return this;
     }
 
-    public <U> FieldCollectionBuilder<T, Parent> complex(TypedPropertyGetter<T, ?> getter,
+    public <U> FieldCollectionBuilder<Type, Parent> complex(TypedPropertyGetter<Type, ?> getter,
         Class<U> c, Consumer<FieldCollectionBuilder<U, ?>> renderer) {
       renderer.accept(complex(getter, c));
       return this;
     }
 
-    public <U> FieldCollectionBuilder<U, T> complex(TypedPropertyGetter<T, U> getter) {
+    public <U> FieldCollectionBuilder<U, Type> complex(TypedPropertyGetter<Type, U> getter) {
       Class<U> c = propertyUtils.getPropertyType(dataClass, getter);
       return complex(getter, c);
     }
 
-    public <U> FieldCollectionBuilder<U, T> complex(TypedPropertyGetter<T, ?> getter,
+    public <U> FieldCollectionBuilder<U, Type> complex(TypedPropertyGetter<Type, ?> getter,
         Class<U> c, boolean summary) {
       String fieldName = propertyUtils.getPropertyName(dataClass, getter);
       if (null == this.rootFieldname) {
@@ -202,12 +203,13 @@ public class FieldCollection<T, Parent> {
       return complex(fieldName, c);
     }
 
-    public <U> FieldCollectionBuilder<U, T> complex(TypedPropertyGetter<T, ?> getter, Class<U> c) {
+    public <U> FieldCollectionBuilder<U, Type> complex(TypedPropertyGetter<Type, ?> getter,
+        Class<U> c) {
       return complex(getter, c, true);
     }
 
-    <U> FieldCollectionBuilder<U, T> complex(String fieldName, Class<U> c) {
-      FieldCollectionBuilder<T, Parent> result = (FieldCollectionBuilder<T, Parent>)
+    <U> FieldCollectionBuilder<U, Type> complex(String fieldName, Class<U> c) {
+      FieldCollectionBuilder<Type, Parent> result = (FieldCollectionBuilder<Type, Parent>)
           FieldCollectionBuilder.builder(event, this, c, propertyUtils);
       complexFields.add(result);
       result.rootFieldname = fieldName;
@@ -215,28 +217,28 @@ public class FieldCollection<T, Parent> {
       if (null != parent) {
         result.fieldDisplayOrder = this.fieldDisplayOrder;
       }
-      return (FieldCollectionBuilder<U, T>) result;
+      return (FieldCollectionBuilder<U, Type>) result;
     }
 
-    public FieldCollectionBuilder<T, Parent> label(String id, String value) {
+    public FieldCollectionBuilder<Type, Parent> label(String id, String value) {
       explicitFields.add(field().id(id).context(DisplayContext.ReadOnly).label(value).readOnly());
       return this;
     }
 
-    public FieldCollectionBuilder<T, Parent> page(String id) {
+    public FieldCollectionBuilder<Type, Parent> page(String id) {
       return this.pageObj(id);
     }
 
-    public FieldCollectionBuilder<T, Parent> page(int id) {
+    public FieldCollectionBuilder<Type, Parent> page(int id) {
       return this.pageObj(String.valueOf(id));
     }
 
-    public FieldCollectionBuilder<T, Parent> previousPage() {
+    public FieldCollectionBuilder<Type, Parent> previousPage() {
       this.pageDisplayOrder--;
       return this;
     }
 
-    private FieldCollectionBuilder<T, Parent> pageObj(String id) {
+    private FieldCollectionBuilder<Type, Parent> pageObj(String id) {
       this.pageId = id;
       this.order = 0;
       this.fieldDisplayOrder = 0;
@@ -244,12 +246,12 @@ public class FieldCollection<T, Parent> {
       return this;
     }
 
-    public FieldCollectionBuilder<T, Parent> pageLabel(String label) {
+    public FieldCollectionBuilder<Type, Parent> pageLabel(String label) {
       this.pageLabels.put(this.pageId.toString(), label);
       return this;
     }
 
-    public EventBuilder<T, ?, ?> eventBuilder() {
+    public EventBuilder<Type, ?, ?> eventBuilder() {
       return event;
     }
   }
