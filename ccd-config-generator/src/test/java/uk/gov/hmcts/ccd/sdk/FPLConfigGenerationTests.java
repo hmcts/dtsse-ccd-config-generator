c2d55a357d46622327f89b3b2a655bc8172c352d
Alex McAusland <alex.mcausland@gmail.com>
2020-04-07 11:13:06 +0100

    Strict CETF comparison

--- ccd-config-generator/src/test/java/uk/gov/hmcts/ccd/sdk/FPLConfigGenerationTests.java
+++ ccd-config-generator/src/test/java/uk/gov/hmcts/ccd/sdk/FPLConfigGenerationTests.java
@@ -154,6 +154,7 @@ public class FPLConfigGenerationTests {
     @Test
     public void generatesAllCaseEventToField() {
         assertResourceFolderMatchesGenerated("CaseEventToFields");
+        assertGeneratedFolderMatchesResource("CaseEventToFields");
     }
 
     @Test
