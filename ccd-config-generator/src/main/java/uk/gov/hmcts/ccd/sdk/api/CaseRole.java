834ab5812c81a0d0387832fa1dbea9d3be092886
Shaun Francis <shaun.francis1@hmcts.net>
2021-03-26 09:19:23 +0000

    Add equals and hashcode to CaseRole class

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/api/CaseRole.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/api/CaseRole.java
@@ -2,9 +2,11 @@ package uk.gov.hmcts.ccd.sdk.api;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Builder
+@EqualsAndHashCode
 public class CaseRole {
 
   String id;
