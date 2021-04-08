ab4d79741e97e3b378d097173cc878434f923b56
Linus Norton <linusnorton@gmail.com>
2021-04-08 14:23:46 +0100

    Add all args constructor annotation

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/Organisation.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/Organisation.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.ccd.sdk.type;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.ccd.sdk.api.ComplexType;
@@ -8,6 +9,7 @@ import uk.gov.hmcts.ccd.sdk.api.ComplexType;
 @Data
 @Builder
 @ComplexType(name = "Organisation", generate = false)
+@AllArgsConstructor
 public class Organisation {
   @JsonProperty("OrganisationID")
   private final String organisationId;
--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/OrganisationPolicy.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/OrganisationPolicy.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.ccd.sdk.type;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.ccd.sdk.api.ComplexType;
@@ -9,6 +10,7 @@ import uk.gov.hmcts.ccd.sdk.api.HasRole;
 @Data
 @ComplexType(name = "OrganisationPolicy", generate = false)
 @Builder
+@AllArgsConstructor
 public class OrganisationPolicy<R extends HasRole> {
 
   @JsonProperty("Organisation")
