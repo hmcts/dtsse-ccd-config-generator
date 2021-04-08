30437f1f1def90914d138e2d76ccbf465db1bfaa
Linus Norton <linusnorton@gmail.com>
2021-04-08 07:33:36 +0100

    Add built in OrganisationPolicy type

---
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/OrganisationPolicy.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.ccd.sdk.type;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+import uk.gov.hmcts.ccd.sdk.api.ComplexType;
+
+@Data
+@ComplexType(name = "OrganisationPolicy", generate = false)
+@Builder
+public class OrganisationPolicy<R> {
+
+  @JsonProperty("Organisation")
+  private final Organisation organisation;
+
+  @JsonProperty("OrgPolicyReference")
+  private final String orgPolicyReference;
+
+  @JsonProperty("OrgPolicyCaseAssignedRole")
+  private final R orgPolicyCaseAssignedRole;
+
+  @Data
+  public static class Organisation {
+    @JsonProperty("OrganisationID")
+    private final String organisationID;
+  }
+}
