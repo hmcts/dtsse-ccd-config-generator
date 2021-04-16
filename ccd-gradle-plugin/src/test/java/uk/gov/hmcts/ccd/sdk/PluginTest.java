e3e83618a936972c4b6ba300c862b2ad70df48b5
Alex McAusland <alex.mcausland@gmail.com>
2021-04-16 16:22:58 +0100

    Bundle the generator inside the Gradle plugin jar
    To avoid having to host the generator lib anywhere (ie. jitpack)

--- ccd-gradle-plugin/src/test/java/uk/gov/hmcts/ccd/sdk/PluginTest.java
+++ ccd-gradle-plugin/src/test/java/uk/gov/hmcts/ccd/sdk/PluginTest.java
@@ -1,16 +1,27 @@
 package uk.gov.hmcts.ccd.sdk;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
 
+
+import com.google.common.collect.SetMultimap;
 import org.gradle.api.Project;
 import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.Test;
 
 public class PluginTest {
-  @Test public void pluginRegistersATask() {
+  @Test
+  public void pluginRegistersATask() {
     Project project = ProjectBuilder.builder().build();
     project.getPlugins().apply("hmcts.ccd.sdk");
 
     assertNotNull(project.getTasks().findByName("generateCCDConfig"));
+  }
+
+  @Test
+  public void pluginExtractsGeneratorDependencies() {
+    SetMultimap<String, String> deps =
+        PomParser.getGeneratorDependencies();
+    assertThat(deps.get("runtimeOnly")).size().isGreaterThan(1);
   }
 }
