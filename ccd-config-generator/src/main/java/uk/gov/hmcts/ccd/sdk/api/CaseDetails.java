4e4813833a51a1f7ec45eb922ef3b8b898c4ddee
Linus Norton <linusnorton@gmail.com>
2021-04-21 14:25:05 +0100

    Add builder for CaseDetails (#120)
    * Add builder for CaseDetails
    
    * sprinkle the love
    
    * sprinkle the love
    
    * sprinkle the love

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/api/CaseDetails.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/api/CaseDetails.java
@@ -3,10 +3,16 @@ package uk.gov.hmcts.ccd.sdk.api;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.ccd.client.model.Classification;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CaseDetails<T, S> {
   private Long id;
 
