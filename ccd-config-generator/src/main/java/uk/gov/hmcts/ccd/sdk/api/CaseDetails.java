0023671c8d49629156776c71d9ac72eb4071b64c
Linus Norton <linusnorton@gmail.com>
2021-04-21 13:26:07 +0100

    sprinkle the love

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/api/CaseDetails.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/api/CaseDetails.java
@@ -6,11 +6,13 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.ccd.client.model.Classification;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class CaseDetails<T, S> {
   private Long id;
 
