867206d55a7f50d4e12072587a163b51c398db70
Tom Elliott <tomelliott@Toms-MBP.home>
2021-04-29 14:23:23 +0100

    Add MoneyGBP CCD type

---
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/MoneyGBP.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.ccd.sdk.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+import uk.gov.hmcts.ccd.sdk.api.ComplexType;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Jacksonized
+@ComplexType(name = "MoneyGBP", generate = false)
+public class MoneyGBP {
+
+  private final String amount;
+}
