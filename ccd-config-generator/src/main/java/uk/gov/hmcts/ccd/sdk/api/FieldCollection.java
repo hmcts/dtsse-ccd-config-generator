2b349c72004747cf870b2723b7431dc0d7aec864
SomeshRA <somesh.acharya1@hmcts.net>
2021-07-21 12:59:24 +0100

    NFDIV-843: Added mandatoryWithLabel and mandatoryWithoutDefaultValue methods

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/api/FieldCollection.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/api/FieldCollection.java
@@ -153,6 +153,21 @@ public class FieldCollection {
       return field(getter, DisplayContext.Mandatory, true);
     }
 
+    public <Value> FieldCollectionBuilder<Type, StateType, Parent> mandatoryWithLabel(
+        TypedPropertyGetter<Type, Value> getter,
+        String caseEventFieldLabel) {
+      return field(
+        getter, DisplayContext.Mandatory, null, true, null, caseEventFieldLabel, null);
+    }
+
+    public <Value> FieldCollectionBuilder<Type, StateType, Parent> mandatoryWithoutDefaultValue(
+        TypedPropertyGetter<Type, Value> getter,
+        String showCondition,
+        String caseEventFieldLabel) {
+      return field(
+        getter, DisplayContext.Mandatory, showCondition, true, null, caseEventFieldLabel, null);
+    }
+
     public FieldCollectionBuilder<Type, StateType, Parent> mandatoryNoSummary(TypedPropertyGetter<Type, ?> getter,
         String showCondition) {
       return field(getter, DisplayContext.Mandatory, showCondition, false);
