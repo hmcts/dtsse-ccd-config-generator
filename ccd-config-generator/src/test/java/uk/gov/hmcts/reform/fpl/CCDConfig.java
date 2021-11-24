4949ca302ef6529bcd3505248bb5dffbc0fd2273
Linus Norton <linusnorton@gmail.com>
2021-11-24 09:58:02 +0000

    Add event enabling conditions

--- ccd-config-generator/src/test/java/uk/gov/hmcts/reform/fpl/CCDConfig.java
+++ ccd-config-generator/src/test/java/uk/gov/hmcts/reform/fpl/CCDConfig.java
@@ -214,6 +214,7 @@ public class CCDConfig implements uk.gov.hmcts.ccd.sdk.api.CCDConfig<CaseData, S
         .grantHistoryOnly(LOCAL_AUTHORITY)
         .grant(CRU, HMCTS_ADMIN)
         .grant(R, CAFCASS)
+        .showCondition("allocatedJudge=\"\"")
         .fields()
         .page("AllocatedJudge")
         .complex(CaseData::getOrganisationPolicy, null, "Event label", "Event hint")
