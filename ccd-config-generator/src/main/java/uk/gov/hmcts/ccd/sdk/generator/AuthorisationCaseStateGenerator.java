a75487cd9703df405d55f7dfe10ceda04a8ab9d9
Linus Norton <linusnorton@gmail.com>
2022-03-11 12:37:06 +0000

    Add forStates overload, add permissions based on .forAllStates again

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/generator/AuthorisationCaseStateGenerator.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/generator/AuthorisationCaseStateGenerator.java
@@ -37,8 +37,7 @@ class AuthorisationCaseStateGenerator<T, S, R extends HasRole> implements Config
         // For state transitions if you have C then you get both states.
         // Otherwise you only need permission for the destination state.
         if (event.getPreState() != event.getPostState()) {
-          if (grants.get(role).contains(Permission.C) && !event.getPreState().isEmpty()
-              && !event.getPostState().isEmpty()) {
+          if (grants.get(role).contains(Permission.C) && !event.getPreState().isEmpty()) {
             addPermissions(config.getStateRolePermissions(), event.getPreState(), role,
                 grants.get(role));
             // They get R only on the destination state.
