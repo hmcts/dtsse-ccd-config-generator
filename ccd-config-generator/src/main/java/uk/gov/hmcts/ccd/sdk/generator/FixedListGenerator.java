9a2d639e8b608f5b4e011b6d89e4e91a0378cefe
Linus Norton <linusnorton@gmail.com>
2022-04-27 07:38:45 +0100

    Use label from CCD annotation on fixed lists (#301)

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/generator/FixedListGenerator.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/generator/FixedListGenerator.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.ccd.sdk.generator;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
+
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import java.io.File;
@@ -7,8 +9,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.ResolvedCCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.api.ComplexType;
 import uk.gov.hmcts.ccd.sdk.api.HasLabel;
 import uk.gov.hmcts.ccd.sdk.api.HasRole;
@@ -17,27 +21,38 @@ import uk.gov.hmcts.ccd.sdk.generator.JsonUtils.AddMissing;
 @Component
 class FixedListGenerator<T, S, R extends HasRole> implements ConfigGenerator<T, S, R> {
 
+  @SneakyThrows
   public void write(File root, ResolvedCCDConfig<T, S, R> config) {
     File dir = root.toPath().resolve("FixedLists").toFile();
     dir.mkdir();
 
-    for (Class c : config.getTypes().keySet()) {
-      ComplexType complexType = (ComplexType) c.getAnnotation(ComplexType.class);
+    for (Class<?> c : config.getTypes().keySet()) {
+      ComplexType complexType = c.getAnnotation(ComplexType.class);
       if (c.isEnum() && (complexType == null || complexType.generate())) {
         List<Map<String, Object>> fields = Lists.newArrayList();
 
         int order = 1;
         for (Object enumConstant : c.getEnumConstants()) {
+          String enumName = ((Enum<?>)enumConstant).name();
+          CCD annotation = c.getField(enumName).getAnnotation(CCD.class);
+
+          // use the enum label field, or the @CCD name, or the @CCD label, or the enumConstant
+          Object label = enumConstant instanceof HasLabel
+              ? ((HasLabel) enumConstant).getLabel()
+              : annotation == null
+                  ? enumConstant
+                  : !isNullOrEmpty(annotation.name())
+                      ? annotation.name()
+                      : !isNullOrEmpty(annotation.label())
+                          ? annotation.label()
+                          : enumConstant;
+
           Map<String, Object> value = Maps.newHashMap();
           fields.add(value);
+          value.put("ListElement", label);
           value.put("LiveFrom", "01/01/2017");
           value.put("ID", c.getSimpleName());
           value.put("ListElementCode", enumConstant);
-          if (enumConstant instanceof HasLabel) {
-            value.put("ListElement", ((HasLabel) enumConstant).getLabel());
-          } else {
-            value.put("ListElement", enumConstant);
-          }
           value.put("DisplayOrder", order++);
         }
 
