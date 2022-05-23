88edfb734e26a81edb1da7dff2f5ce7539b18884
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2022-05-23 09:11:00 +0100

    Use deserializer to format milliseconds in case ending zeros are removed while persisting in CCD (#321)

---
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/deserializer/LocalDateTimeDeserializer.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.ccd.sdk.deserializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+
+public class LocalDateTimeDeserializer extends StdDeserializer<LocalDateTime> {
+
+  private static final long serialVersionUID = 1L;
+
+  protected LocalDateTimeDeserializer() {
+    super(LocalDateTime.class);
+  }
+
+  
+  @Override
+  public LocalDateTime deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+    var dateString = jp.readValueAs(String.class);
+    DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+            .appendPattern("yyyy-MM-dd'T'HH:mm:ss")
+            .appendFraction(ChronoField.MILLI_OF_SECOND, 2, 3, true)
+            .toFormatter();
+    return LocalDateTime.parse(dateString, formatter);
+  }
+}
