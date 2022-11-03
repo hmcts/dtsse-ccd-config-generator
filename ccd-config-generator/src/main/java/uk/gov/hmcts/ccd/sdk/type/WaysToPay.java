d5330f4e6e5d533f3e4d7967ca762508d258f6f6
Sairam Kantheti <sai.kantheti@hmcts.net>
2022-11-03 13:08:55 +0000

    added JsonIgnoreTYpe annotation as it is not a type that directly used in casedata

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/WaysToPay.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/WaysToPay.java
@@ -2,9 +2,11 @@ package uk.gov.hmcts.ccd.sdk.type;
 
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
 import uk.gov.hmcts.ccd.sdk.api.ComplexType;
 
 
+@JsonIgnoreType
 @ComplexType(name = "WaysToPay")
 public class WaysToPay {
 
