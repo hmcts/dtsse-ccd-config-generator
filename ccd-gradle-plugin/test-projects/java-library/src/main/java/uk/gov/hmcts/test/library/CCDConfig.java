fb41e208eeef3281d362b9534d9f1fa4ec607f0d
Alex McAusland <a.m@user.com>
2023-01-09 15:10:55 +0000

    Declare ccd client as an API level dependency
    We use the ccd client types in our APIs so it must be an API level
    dependency so consumers can access those types.

--- ccd-gradle-plugin/test-projects/java-library/src/main/java/uk/gov/hmcts/test/library/CCDConfig.java
+++ ccd-gradle-plugin/test-projects/java-library/src/main/java/uk/gov/hmcts/test/library/CCDConfig.java
@@ -1,12 +1,35 @@
 package uk.gov.hmcts.test.library;
 
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+
+import static uk.gov.hmcts.ccd.sdk.api.Permission.CRU;
 
 @Component
 public class CCDConfig implements uk.gov.hmcts.ccd.sdk.api.CCDConfig<CaseData, State, Role> {
   @Override
   public void configure(ConfigBuilder<CaseData, State, Role> builder) {
     builder.caseType("test", "test", "test");
+      builder
+            .event("submit-case")
+        .forAllStates()
+      .name("Submit case")
+      .description("Submit case")
+      .aboutToSubmitCallback(this::aboutToSubmit)
+      .submittedCallback(this::submitted)
+      .fields()
+      .mandatory(CaseData::getDocuments)
+      .done();
+
+  }
+
+  private uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse submitted(CaseDetails<CaseData, State> caseDataStateCaseDetails, CaseDetails<CaseData, State> caseDataStateCaseDetails1) {
+    return null;
+  }
+
+  private AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> caseDataStateCaseDetails, CaseDetails<CaseData, State> caseDataStateCaseDetails1) {
+    return null;
   }
 }
