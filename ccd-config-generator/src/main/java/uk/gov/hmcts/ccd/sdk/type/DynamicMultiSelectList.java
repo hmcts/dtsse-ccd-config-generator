22a2c72ad8cccfa33e384e15243432c04a658430
hmctscivil <version1rhel@hmctscivil.mshome.net>
2023-04-14 12:43:27 +0100

    ST-645 DynamicMultiSelectList object created

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/DynamicMultiSelectList.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/DynamicMultiSelectList.java
@@ -1,14 +1,17 @@
 package uk.gov.hmcts.ccd.sdk.type;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
 import uk.gov.hmcts.ccd.sdk.api.ComplexType;
 
 /**
@@ -19,50 +22,50 @@ import uk.gov.hmcts.ccd.sdk.api.ComplexType;
 @Data
 @ComplexType(generate = false)
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Jacksonized
 public class DynamicMultiSelectList {
 
   /**
-   * The selected values for the dropdown.
+   * The selected value for the multiselect options.
    */
-  private List<DynamicListElement> values;
+  @JsonProperty("value")
+  private List<DynamicListElement> value;
 
   /**
-   * List of options for the dropdown.
+   * List of options for the multiselect options.
    */
   @JsonProperty("list_items")
   private List<DynamicListElement> listItems;
 
   @JsonCreator
   public DynamicMultiSelectList(
-      @JsonProperty("values") List<DynamicListElement> values,
+      @JsonProperty("value")  List<DynamicListElement>  value,
       @JsonProperty("list_items") List<DynamicListElement> listItems
   ) {
-    this.values = values;
+    this.value = value;
     this.listItems = listItems;
   }
 
-  /**
-   * Converts a list of elements to the appropriate structure to then be handled correctly by CCD.
-   *
-   * @param elements elements to convert into options for the dropdown
-   * @param selected a {@link DynamicListElement} that will be the default selected element of the list
-   * @param <T>      a class that implements {@link DynamicElementIndicator#toDynamicElement()}
-   * @return a {@link DynamicMultiSelectList} to be sent to CCD
-   */
   public static <T extends DynamicElementIndicator> DynamicMultiSelectList toDynamicMultiSelectList(
       List<T> elements,
       List<DynamicListElement> selected) {
     List<DynamicListElement> items = elements.stream()
         .map(DynamicElementIndicator::toDynamicElement).collect(Collectors.toList());
-    return DynamicMultiSelectList.builder().listItems(items).values(selected).build();
+    return DynamicMultiSelectList.builder().listItems(items).value(selected).build();
   }
 
-  public String getValueLabel(DynamicListElement element) {
-    return element == null ? null : element.getLabel();
+  @JsonIgnore
+  public String getValueLabel() {
+    return value == null ? null : value.toString();
   }
 
-  public UUID getValueCode(DynamicListElement element) {
-    return element == null ? null : element.getCode();
+  @JsonIgnore
+  public UUID getValueCodeAsUuid() {
+    return Optional.ofNullable(getValueCode()).map(UUID::fromString).orElse(null);
   }
 
+  @JsonIgnore
+  public String getValueCode() {
+    return value == null ? null : value.toString();
+  }
 }
