18fc5dc0275ff0b5c0f13efa528e03f087e4e727
Linus Norton <linusnorton@gmail.com>
2023-07-21 10:09:29 +0100

    Revert "ST-475 Link type changes"

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/CaseLink.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/CaseLink.java
@@ -1,16 +1,13 @@
 package uk.gov.hmcts.ccd.sdk.type;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.time.LocalDate;
-import java.util.Set;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.ComplexType;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Builder
 @Data
 @ComplexType(name = "CaseLink", generate = false)
@@ -19,12 +16,8 @@ public class CaseLink {
   @JsonProperty("CaseReference")
   private String caseReference;
 
-  @JsonProperty("ReasonForLink")
-  private Set<LinkReason> reasonForLink;
-
-  @JsonProperty("CreatedDateTime")
-  private LocalDate createdDateTime;
-
-  @JsonProperty("CaseType")
-  private String caseType;
+  @JsonCreator
+  public CaseLink(@JsonProperty("CaseReference") String caseReference) {
+    this.caseReference = caseReference;
+  }
 }
