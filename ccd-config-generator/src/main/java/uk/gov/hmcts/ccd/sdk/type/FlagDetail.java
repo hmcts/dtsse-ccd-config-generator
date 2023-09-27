574cb99c30d2bc6246d13bee9ae37eb69a012b6f
sjamihmcts <santoshini.jami@hmcts.net>
2023-09-27 12:04:22 +0100

    ST-1048: Changes required for Case Flags functionality - Sptribs

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/FlagDetail.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/FlagDetail.java
@@ -1,16 +1,19 @@
 package uk.gov.hmcts.ccd.sdk.type;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.time.LocalDateTime;
-import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.ComplexType;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @NoArgsConstructor
-@Builder
+@AllArgsConstructor
+@Builder(toBuilder = true)
 @Data
 @ComplexType(name = "FlagDetail", generate = false)
 @SuppressWarnings("java:S117")
@@ -47,16 +50,18 @@ public class FlagDetail {
   private String flagUpdateComment;
 
   @JsonProperty("dateTimeModified")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
   private LocalDateTime dateTimeModified;
 
   @JsonProperty("dateTimeCreated")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
   private LocalDateTime dateTimeCreated;
 
   @JsonProperty("path")
   private List<ListValue<String>> path;
 
-  @JsonProperty("hearingRelated")
-  private YesOrNo hearingRelated;
+  @JsonProperty("hearingRelevant")
+  private YesOrNo hearingRelevant;
 
   @JsonProperty("flagCode")
   private String flagCode;
@@ -67,43 +72,4 @@ public class FlagDetail {
   @JsonProperty("availableExternally")
   private YesOrNo availableExternally;
 
-
-  @JsonCreator
-  public FlagDetail(@JsonProperty("name") String name,
-                    @JsonProperty("name_cy") String nameCy,
-                    @JsonProperty("subTypeValue") String subTypeValue,
-                    @JsonProperty("subTypeValue_cy") String subTypeValueCy,
-                    @JsonProperty("subTypeKey") String subTypeKey,
-                    @JsonProperty("otherDescription") String otherDescription,
-                    @JsonProperty("otherDescription_cy") String otherDescriptionCy,
-                    @JsonProperty("flagComment") String flagComment,
-                    @JsonProperty("flagComment_cy") String flagCommentCy,
-                    @JsonProperty("flagUpdateComment") String flagUpdateComment,
-                    @JsonProperty("dateTimeModified") LocalDateTime dateTimeModified,
-                    @JsonProperty("dateTimeCreated") LocalDateTime dateTimeCreated,
-                    @JsonProperty("path") List<ListValue<String>>  path,
-                    @JsonProperty("hearingRelated") YesOrNo hearingRelated,
-                    @JsonProperty("flagCode") String flagCode,
-                    @JsonProperty("status") String status,
-                    @JsonProperty("availableExternally") YesOrNo availableExternally
-  ) {
-    this.name = name;
-    this.nameCy = nameCy;
-    this.subTypeValue = subTypeValue;
-    this.subTypeValueCy = subTypeValueCy;
-    this.subTypeKey = subTypeKey;
-    this.otherDescription = otherDescription;
-    this.otherDescriptionCy = otherDescriptionCy;
-    this.flagComment = flagComment;
-    this.flagCommentCy = flagCommentCy;
-    this.flagUpdateComment = flagUpdateComment;
-    this.dateTimeCreated = dateTimeCreated;
-    this.dateTimeModified = dateTimeModified;
-    this.path = path;
-    this.hearingRelated = hearingRelated;
-    this.flagCode = flagCode;
-    this.status = status;
-    this.availableExternally = availableExternally;
-
-  }
 }
--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/FlagLauncher.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/FlagLauncher.java
@@ -1,7 +1,9 @@
 package uk.gov.hmcts.ccd.sdk.type;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import uk.gov.hmcts.ccd.sdk.api.ComplexType;
 
-@ComplexType
+@ComplexType(generate = false)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FlagLauncher {
 }
