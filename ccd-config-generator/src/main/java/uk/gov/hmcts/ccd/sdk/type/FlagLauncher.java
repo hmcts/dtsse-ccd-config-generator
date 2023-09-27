e17a43e57b309e7512a6e0e230abb2541baee5f4
sjamihmcts <santoshini.jami@hmcts.net>
2023-09-27 12:37:14 +0100

    ST-1048: Removed as it is not required

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/FlagLauncher.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/FlagLauncher.java
@@ -1,9 +1,7 @@
 package uk.gov.hmcts.ccd.sdk.type;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import uk.gov.hmcts.ccd.sdk.api.ComplexType;
 
 @ComplexType(generate = false)
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class FlagLauncher {
 }
