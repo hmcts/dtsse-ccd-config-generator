94064f666f00f04cab31d007700a432fccffcc5b
sjamihmcts <santoshini.jami@hmcts.net>
2023-09-27 12:49:13 +0100

    ST-1048: Added the constructor as per review comments

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/FlagDetail.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/FlagDetail.java
@@ -1,18 +1,18 @@
 package uk.gov.hmcts.ccd.sdk.type;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.time.LocalDateTime;
-import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.ComplexType;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder(toBuilder = true)
+@Builder
 @Data
 @ComplexType(name = "FlagDetail", generate = false)
 @SuppressWarnings("java:S117")
@@ -71,4 +71,42 @@ public class FlagDetail {
   @JsonProperty("availableExternally")
   private YesOrNo availableExternally;
 
+  @JsonCreator
+  public FlagDetail(@JsonProperty("name") String name,
+                    @JsonProperty("name_cy") String nameCy,
+                    @JsonProperty("subTypeValue") String subTypeValue,
+                    @JsonProperty("subTypeValue_cy") String subTypeValueCy,
+                    @JsonProperty("subTypeKey") String subTypeKey,
+                    @JsonProperty("otherDescription") String otherDescription,
+                    @JsonProperty("otherDescription_cy") String otherDescriptionCy,
+                    @JsonProperty("flagComment") String flagComment,
+                    @JsonProperty("flagComment_cy") String flagCommentCy,
+                    @JsonProperty("flagUpdateComment") String flagUpdateComment,
+                    @JsonProperty("dateTimeModified") LocalDateTime dateTimeModified,
+                    @JsonProperty("dateTimeCreated") LocalDateTime dateTimeCreated,
+                    @JsonProperty("path") List<ListValue<String>>  path,
+                    @JsonProperty("hearingRelevant") YesOrNo hearingRelevant,
+                    @JsonProperty("flagCode") String flagCode,
+                    @JsonProperty("status") String status,
+                    @JsonProperty("availableExternally") YesOrNo availableExternally
+  ) {
+    this.name = name;
+    this.nameCy = nameCy;
+    this.subTypeValue = subTypeValue;
+    this.subTypeValueCy = subTypeValueCy;
+    this.subTypeKey = subTypeKey;
+    this.otherDescription = otherDescription;
+    this.otherDescriptionCy = otherDescriptionCy;
+    this.flagComment = flagComment;
+    this.flagCommentCy = flagCommentCy;
+    this.flagUpdateComment = flagUpdateComment;
+    this.dateTimeCreated = dateTimeCreated;
+    this.dateTimeModified = dateTimeModified;
+    this.path = path;
+    this.hearingRelevant = hearingRelevant;
+    this.flagCode = flagCode;
+    this.status = status;
+    this.availableExternally = availableExternally;
+
+  }
 }
