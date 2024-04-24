3eb652b1d4c9a8918c0f7e56b5c77d42d04779c2
adamg-hmcts <adam.goddard@justice.gov.uk>
2024-04-24 09:04:20 +0100

    Remove unused imports

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/PreviousOrganisationCollectionItem.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/PreviousOrganisationCollectionItem.java
@@ -1,13 +1,7 @@
 package uk.gov.hmcts.ccd.sdk.type;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
