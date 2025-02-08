78d2fa92992f5682f80dc8413d745d3ca157bca9
Alex McAusland <a.m@user.com>
2025-02-08 20:53:17 +0000

    tidy

--- data-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/CCDEventListener.java
+++ data-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/CCDEventListener.java
@@ -6,7 +6,6 @@ import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse
 
 public interface CCDEventListener {
   AboutToStartOrSubmitResponse aboutToSubmit(CallbackRequest request);
-  void submitted(CallbackRequest request);
   boolean hasSubmittedCallbackForEvent(String event);
   boolean hasAboutToSubmitCallbackForEvent(String event);
 }
