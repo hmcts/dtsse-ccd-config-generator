12abacaa171932f995daa9ffcb7eb570b9432573
Alex McAusland <a.m@user.com>
2025-05-19 16:17:40 +0100

    fix decentralisd start events for new cases

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/api/EventPayload.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/api/EventPayload.java
@@ -2,5 +2,7 @@ package uk.gov.hmcts.ccd.sdk.api;
 
 import org.springframework.util.MultiValueMap;
 
-public record EventPayload<T, S>(long caseReference, T caseData, MultiValueMap<String, String> urlParams) {
+// TODO: caseReference is nullable currently.
+// We should have a separate start event for create case start events that do not have a case reference.
+public record EventPayload<T, S>(Long caseReference, T caseData, MultiValueMap<String, String> urlParams) {
 }
