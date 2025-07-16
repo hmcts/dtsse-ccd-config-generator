fec666a4d2ed62fb48ef632c957237ad7a8e0959
Alex McAusland <a.m@user.com>
2025-07-16 12:09:26 +0100

    snake case

--- data-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/POCCaseEvent.java
+++ data-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/POCCaseEvent.java
@@ -1,10 +1,13 @@
 package uk.gov.hmcts.ccd.sdk;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 import java.util.Map;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Data
 public class POCCaseEvent {
 
