94fc513ff718c98c68fdb2d824f91727da09391d
Alex McAusland <a.m@user.com>
2025-09-18 23:26:22 +0100

    docs

--- decentralised-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/External.java
+++ decentralised-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/External.java
@@ -3,6 +3,10 @@ package uk.gov.hmcts.ccd.sdk;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Marker annotation used by the decentralised runtime to indicate that a CCD field
+ * should be treated as external-facing only and stripped from the main case_data json blob.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface External {
 }
--- decentralised-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/FilterExternalFieldsInspector.java
+++ decentralised-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/FilterExternalFieldsInspector.java
@@ -3,8 +3,20 @@ package uk.gov.hmcts.ccd.sdk;
 import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 
+/**
+ * Jackson annotation inspector that filters out properties marked with
+ * {@link External} during serialisation.
+ *
+ * <p>Used by a dedicated {@code ObjectMapper} instance in the decentralised runtime
+ * (see usages in the repository) to ensure fields intended for external-only
+ * consumption are stripped from the case_data.data blob.
+ */
 public class FilterExternalFieldsInspector extends JacksonAnnotationIntrospector {
 
+  /**
+   * Signals to Jackson that a property should be ignored when it is annotated
+   * with {@link External}. Falls back to the default behaviour otherwise.
+   */
   @Override
   public boolean hasIgnoreMarker(AnnotatedMember a) {
     if (a.hasAnnotation(External.class)) {
