9adebddcd45417ba363eed3a7bfbcfefcea69704
Alex McAusland <a.m@user.com>
2025-10-02 22:25:53 +0100

    Drop CHANGE_ID wiring for e2e case type

--- test-projects/e2e/src/main/java/uk/gov/hmcts/divorce/divorcecase/NoFaultDivorce.java
+++ test-projects/e2e/src/main/java/uk/gov/hmcts/divorce/divorcecase/NoFaultDivorce.java
@@ -8,8 +8,6 @@ import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
-import static java.lang.System.getenv;
-import static java.util.Optional.ofNullable;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_1_SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_2_SOLICITOR;
 
@@ -22,25 +20,19 @@ public class NoFaultDivorce implements CCDConfig<CaseData, State, UserRole> {
     public static final String JURISDICTION = "DIVORCE";
 
     public static String getCaseType() {
-        return ofNullable(getenv().get("CHANGE_ID"))
-            .map(num -> CASE_TYPE + "-" + num)
-            .orElse(CASE_TYPE);
+        return CASE_TYPE;
     }
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
-        configBuilder.setCallbackHost(getenv().getOrDefault("CASE_API_URL", "http://localhost:4013"));
+        configBuilder.setCallbackHost("http://localhost:4013");
 
-        var caseTypeDescription = ofNullable(getenv().get("CHANGE_ID"))
-            .map(num -> CASE_TYPE_DESCRIPTION + "-" + num)
-            .orElse(CASE_TYPE_DESCRIPTION);
-
-        configBuilder.caseType(getCaseType(), caseTypeDescription, "Handling of the dissolution of marriage");
+        configBuilder.caseType(getCaseType(), CASE_TYPE_DESCRIPTION, "Handling of the dissolution of marriage");
         configBuilder.jurisdiction(JURISDICTION, "Family Divorce", "Family Divorce: dissolution of marriage");
         configBuilder.omitHistoryForRoles(APPLICANT_1_SOLICITOR, APPLICANT_2_SOLICITOR);
 
         // to shutter the service within xui uncomment this line
         // configBuilder.shutterService();
-        log.info("Building definition for " + getenv().getOrDefault("ENVIRONMENT", ""));
+        log.info("Building definition for local cftlib e2e tests");
     }
 }
