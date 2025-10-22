5fbfd849eb0e5a2a38e74585c4e82cd900b908cf
Alex McAusland <a.m@user.com>
2025-10-22 06:41:34 +0100

    docs

--- sdk/decentralised-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/CaseView.java
+++ sdk/decentralised-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/CaseView.java
@@ -1,21 +1,21 @@
 package uk.gov.hmcts.ccd.sdk;
 
 /**
- * Projection hook that allows services to reshape CCD case data for the UI.
+ * Provides case data to CCD when CCD loads cases.
  * <p>
  * Decentralised services typically implement the request-only overload and ignore the JSON blob,
- * while legacy services can override the request-plus-blob overload to work directly with the blob.
+ * while legacy services can override the request-plus-blob overload to continue working with the blob.
  *
- * @param <CaseType>  the domain model returned to CCD
- * @param <StateType> the strongly-typed representation of the CCD state
+ * @param <ViewType> domain view returned to CCD
+ * @param <StateType> typed enum representing possible CCD states
  */
-public interface CaseView<CaseType, StateType extends Enum<StateType>> {
+public interface CaseView<ViewType, StateType extends Enum<StateType>> {
 
   /**
    * @param request encapsulated request information
    * @return the projected case data
    */
-  default CaseType getCase(CaseViewRequest<StateType> request) {
+  default ViewType getCase(CaseViewRequest<StateType> request) {
     throw new UnsupportedOperationException(
         "CaseView implementations must override getCase(CaseViewRequest) or "
             + "getCase(CaseViewRequest, CaseType)");
@@ -28,7 +28,7 @@ public interface CaseView<CaseType, StateType extends Enum<StateType>> {
    * @param blobCase case data deserialised from the blob
    * @return the projected case data
    */
-  default CaseType getCase(CaseViewRequest<StateType> request, CaseType blobCase) {
+  default ViewType getCase(CaseViewRequest<StateType> request, ViewType blobCase) {
     return getCase(request);
   }
 }
