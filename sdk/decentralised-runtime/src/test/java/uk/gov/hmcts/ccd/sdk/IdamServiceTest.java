0698db30e92cc67305ff364596cca18a0c20ecea
Alex McAusland <a.m@user.com>
2025-10-29 21:24:09 +0000

    cache userinfo lookups in decentralised runtime

---
+++ sdk/decentralised-runtime/src/test/java/uk/gov/hmcts/ccd/sdk/IdamServiceTest.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.ccd.sdk;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class IdamServiceTest {
+
+  @Test
+  void shouldCacheUserDetailsForBearerToken() {
+    var userInfo = new UserInfo("sub", "uid", "name", "given", "family", java.util.List.of("caseworker"));
+    var idamClient = new StubIdamClient(userInfo);
+    var idamService = new IdamService(idamClient, 10, 60);
+
+    var firstCall = idamService.retrieveUser("token");
+    var secondCall = idamService.retrieveUser("token");
+
+    assertThat(firstCall).isSameAs(secondCall);
+    assertThat(firstCall.getUserDetails()).isEqualTo(userInfo);
+    assertThat(idamClient.callCount).isEqualTo(1);
+    assertThat(idamClient.lastToken).isEqualTo("Bearer token");
+  }
+
+  private static class StubIdamClient extends IdamClient {
+    private int callCount;
+    private String lastToken;
+    private final UserInfo userInfo;
+
+    StubIdamClient(UserInfo userInfo) {
+      super(null, null);
+      this.userInfo = userInfo;
+    }
+
+    @Override
+    public UserInfo getUserInfo(String authorisation) {
+      callCount++;
+      lastToken = authorisation;
+      return userInfo;
+    }
+  }
+}
