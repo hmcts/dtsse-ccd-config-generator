251a3705776c4f3382f9ced6212879a83c50a4e9
Alex McAusland <a.m@user.com>
2025-12-02 20:36:03 +0000

    pull definitions into a registry

---
+++ sdk/decentralised-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/impl/DefinitionRegistry.java
@@ -0,0 +1,60 @@
+package uk.gov.hmcts.ccd.sdk.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.domain.model.definition.CaseTypeDefinition;
+
+@Component
+@Slf4j
+class DefinitionRegistry {
+
+  private final ObjectMapper mapper;
+  private Map<String, CaseTypeDefinition> definitions = Map.of();
+
+  DefinitionRegistry(@Qualifier("ccd_mapper") ObjectMapper definitionMapper) {
+    this.mapper = definitionMapper;
+  }
+
+  Optional<CaseTypeDefinition> find(String caseTypeId) {
+    return Optional.ofNullable(loadDefinitions().get(caseTypeId));
+  }
+
+  /**
+   * Lazily loads case type definitions from the generated snapshot directory.
+   */
+  @SneakyThrows
+  synchronized Map<String, CaseTypeDefinition> loadDefinitions() {
+    if (!this.definitions.isEmpty()) {
+      return this.definitions;
+    }
+
+    var loaded = new HashMap<String, CaseTypeDefinition>();
+    File[] jsonFiles = new File("build/cftlib/definition-snapshots")
+        .listFiles((dir, name) -> name.endsWith(".json"));
+
+    if (jsonFiles != null) {
+      for (File file : jsonFiles) {
+        String fileNameWithoutExtension = file.getName().substring(0, file.getName().lastIndexOf("."));
+        CaseTypeDefinition definition = mapper.readValue(file, CaseTypeDefinition.class);
+        loaded.put(fileNameWithoutExtension, definition);
+      }
+    }
+
+    if (loaded.isEmpty()) {
+      log.warn("DefinitionRegistry: no definition snapshots found");
+      this.definitions = Map.of();
+      return this.definitions;
+    }
+
+    this.definitions = Map.copyOf(loaded);
+    log.info("DefinitionRegistry loaded {} case type definitions", this.definitions.size());
+    return this.definitions;
+  }
+}
