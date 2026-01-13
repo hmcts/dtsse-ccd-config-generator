244f34e3b68ce650994f85d0150c0e514814ad96
Alex McAusland <a.m@user.com>
2026-01-13 20:03:09 +0000

    rename

--- sdk/ccd-task-management/src/main/java/uk/gov/hmcts/ccd/sdk/taskmanagement/TaskManagementApiResponse.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.hmcts.ccd.sdk.taskmanagement;
-
-public record TaskManagementApiResponse(Integer statusCode, String body, TaskCreateResponse task) {
-  public boolean isSuccess() {
-    return statusCode != null && (statusCode == 200 || statusCode == 201);
-  }
-}
