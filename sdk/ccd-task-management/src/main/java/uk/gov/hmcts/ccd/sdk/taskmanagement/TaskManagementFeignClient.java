73762bc47aaf6641bcef482996ba639b788f4ae3
Alex McAusland <a.m@user.com>
2026-01-13 16:50:43 +0000

    tidy

---
+++ sdk/ccd-task-management/src/main/java/uk/gov/hmcts/ccd/sdk/taskmanagement/TaskManagementFeignClient.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.ccd.sdk.taskmanagement;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(
+    name = "task-management-api",
+    url = "${task-management.api.url}",
+    configuration = TaskManagementFeignConfig.class
+)
+public interface TaskManagementFeignClient {
+
+  @PostMapping(value = "/task/create", consumes = MediaType.APPLICATION_JSON_VALUE)
+  ResponseEntity<TaskCreateResponse> createTask(@RequestBody TaskCreateRequest payload);
+}
