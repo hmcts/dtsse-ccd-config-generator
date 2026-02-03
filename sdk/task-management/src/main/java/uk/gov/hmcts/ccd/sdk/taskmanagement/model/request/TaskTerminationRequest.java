e084c66804c27bb7cdc2161be043c23893e4a9c0
EllisD-B <ellis.domengerboyce@hmcts.net>
2026-02-03 16:41:15 +0000

    add snake case to termination

--- sdk/task-management/src/main/java/uk/gov/hmcts/ccd/sdk/taskmanagement/model/request/TaskTerminationRequest.java
+++ sdk/task-management/src/main/java/uk/gov/hmcts/ccd/sdk/taskmanagement/model/request/TaskTerminationRequest.java
@@ -1,8 +1,12 @@
 package uk.gov.hmcts.ccd.sdk.taskmanagement.model.request;
 
 import java.util.List;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record TaskTerminationRequest(String action, List<String> taskIds) {
 }
