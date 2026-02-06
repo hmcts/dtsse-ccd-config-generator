c64251108610d01db70f9ee224d321415dc89b8d
skandala-cgi <sita.kandala@hmcts.net>
2026-02-06 09:02:49 +0000

    TMDPC-20 initial commit

--- sdk/task-management/src/main/java/uk/gov/hmcts/ccd/sdk/taskmanagement/model/request/TaskReconfigureRequest.java
+++ sdk/task-management/src/main/java/uk/gov/hmcts/ccd/sdk/taskmanagement/model/request/TaskReconfigureRequest.java
@@ -5,9 +5,10 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import java.util.List;
 import uk.gov.hmcts.ccd.sdk.taskmanagement.model.TaskPayload;
 import lombok.Builder;
+import uk.gov.hmcts.ccd.sdk.taskmanagement.model.TaskReconfigurePayload;
 
 @Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public record TaskReconfigureRequest(String caseId, List<TaskPayload> tasks) {
+public record TaskReconfigureRequest(List<TaskReconfigurePayload> tasks) {
 }
 
