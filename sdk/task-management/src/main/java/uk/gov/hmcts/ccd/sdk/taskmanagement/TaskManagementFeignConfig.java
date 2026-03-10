48db2ee2c7e67ade88a1aefb7efab41ca0590285
Alex McAusland <a.m@user.com>
2026-03-10 10:47:33 +0000

    avoid auto applying feign configuration to all application feign clients, scope it only to the task mgmt one

--- sdk/task-management/src/main/java/uk/gov/hmcts/ccd/sdk/taskmanagement/TaskManagementFeignConfig.java
+++ sdk/task-management/src/main/java/uk/gov/hmcts/ccd/sdk/taskmanagement/TaskManagementFeignConfig.java
@@ -9,10 +9,8 @@ import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
 import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 
-@Configuration
 public class TaskManagementFeignConfig {
 
   @Bean
