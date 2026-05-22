a087f81f475e07ef86a32ff151cfa428abbc2404
Alex McAusland <a.m@user.com>
2026-05-22 11:16:31 +0100

    Add native NoC validate and submit support

--- sdk/ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/api/NoticeOfChange.java
+++ sdk/ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/api/NoticeOfChange.java
@@ -2,17 +2,26 @@ package uk.gov.hmcts.ccd.sdk.api;
 
 import com.google.common.collect.Lists;
 import java.util.List;
+import java.util.function.BiFunction;
 import lombok.Data;
+import uk.gov.hmcts.ccd.sdk.api.noc.NocAnswersRequest;
+import uk.gov.hmcts.ccd.sdk.api.noc.NocAnswersResponse;
+import uk.gov.hmcts.ccd.sdk.api.noc.NocEndpoint;
+import uk.gov.hmcts.ccd.sdk.api.noc.NocSubmissionResponse;
+import uk.gov.hmcts.ccd.sdk.api.noc.NocSubmitContext;
 
 @Data
 public class NoticeOfChange<T, R extends HasRole> {
 
   private final List<ChallengeQuestion<T, R>> challenges = Lists.newArrayList();
+  private NocEndpoint endpoint;
 
   public static class NoticeOfChangeBuilder<T, R extends HasRole> {
     private final NoticeOfChange<T, R> noticeOfChange = new NoticeOfChange<>();
     private final Class<T> model;
     private final PropertyUtils propertyUtils;
+    private BiFunction<NocSubmitContext, NocAnswersRequest, NocAnswersResponse> validationHandler;
+    private BiFunction<NocSubmitContext, NocAnswersRequest, NocSubmissionResponse> submissionHandler;
 
     public NoticeOfChangeBuilder(Class<T> model, PropertyUtils propertyUtils) {
       this.model = model;
@@ -25,7 +34,26 @@ public class NoticeOfChange<T, R extends HasRole> {
       return new ChallengeQuestion.ChallengeBuilder<>(challenge, model, propertyUtils);
     }
 
-    public NoticeOfChange<T, R> build() {
+    public NoticeOfChangeBuilder<T, R> validate(
+        BiFunction<NocSubmitContext, NocAnswersRequest, NocAnswersResponse> handler) {
+      this.validationHandler = handler;
+      return this;
+    }
+
+    public NoticeOfChangeBuilder<T, R> submit(
+        BiFunction<NocSubmitContext, NocAnswersRequest, NocSubmissionResponse> handler) {
+      this.submissionHandler = handler;
+      return this;
+    }
+
+    public NoticeOfChange<T, R> build(String caseTypeId) {
+      if (validationHandler != null || submissionHandler != null) {
+        noticeOfChange.endpoint = NocEndpoint.builder()
+            .caseTypeId(caseTypeId)
+            .validate(validationHandler)
+            .submit(submissionHandler)
+            .build();
+      }
       return noticeOfChange;
     }
   }
--- sdk/ccd-config-generator/src/test/java/uk/gov/hmcts/ccd/sdk/UnitTest.java
+++ sdk/ccd-config-generator/src/test/java/uk/gov/hmcts/ccd/sdk/UnitTest.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.junit.Test;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.noc.NocOrganisation;
+import uk.gov.hmcts.ccd.sdk.api.noc.NocSubmissionResponse;
 import uk.gov.hmcts.example.missingcomplex.Applicant;
 import uk.gov.hmcts.example.missingcomplex.MissingComplex;
 import uk.gov.hmcts.reform.fpl.enums.State;
@@ -42,5 +44,27 @@ public class UnitTest {
     ConfigResolver<MissingComplex, State, UserRole> generator = new ConfigResolver<>(List.of(new MissingBug()));
     ResolvedCCDConfig<MissingComplex, State, UserRole> resolved = generator.resolveCCDConfig();
     assertThat(resolved.types).containsKeys(Applicant.class);
+  }
+
+  @Test
+  public void configBuilderCanConfigureNoticeOfChangeRuntimeHandlers() {
+    class NocConfig implements CCDConfig<CaseData, State, UserRole> {
+      @Override
+      public void configure(ConfigBuilder<CaseData, State, UserRole> builder) {
+        builder.caseType("TEST", "Test", "Test case type");
+        builder.noticeOfChange()
+            .validate((context, request) -> uk.gov.hmcts.ccd.sdk.api.noc.NocAnswersResponse.verified(
+                new NocOrganisation("ORG1", "Org 1")
+            ))
+            .submit((context, request) -> NocSubmissionResponse.approved("[DEFENDANTSOLICITOR]"));
+      }
+    }
+
+    ConfigResolver<CaseData, State, UserRole> generator = new ConfigResolver<>(List.of(new NocConfig()));
+    ResolvedCCDConfig<CaseData, State, UserRole> resolved = generator.resolveCCDConfig();
+
+    assertThat(resolved.getNoticeOfChange().getEndpoint()).isNotNull();
+    assertThat(resolved.getNoticeOfChange().getEndpoint().caseTypeId()).isEqualTo("TEST");
+    assertThat(resolved.getNoticeOfChange().getEndpoint().isAuthorisedService("xui_webapp")).isTrue();
   }
 }
