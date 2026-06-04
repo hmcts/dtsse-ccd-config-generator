fcbf244f57cfd8202126146cae4163c9e7e184e7
Alex McAusland <a.m@user.com>
2026-06-04 08:54:26 +0100

    Revert "shape"
    This reverts commit 201bd396d78af1f5d26b06ff6f5340002f0e74b0.

--- test-projects/e2e/src/main/java/uk/gov/hmcts/divorce/jsonlegacy/JsonLegacyCcdConfig.java
+++ test-projects/e2e/src/main/java/uk/gov/hmcts/divorce/jsonlegacy/JsonLegacyCcdConfig.java
@@ -1,9 +1,12 @@
 package uk.gov.hmcts.divorce.jsonlegacy;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
-import uk.gov.hmcts.ccd.sdk.json.JsonBackedCCDConfigFactory;
+import uk.gov.hmcts.ccd.sdk.impl.json.JsonCallbackAdapterFactory;
+import uk.gov.hmcts.ccd.sdk.json.JsonBackedCCDConfig;
 import uk.gov.hmcts.divorce.divorcecase.NoFaultDivorce;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
@@ -15,7 +18,15 @@ public class JsonLegacyCcdConfig {
   private static final String JSON_ROOT = "classpath:json-ccd-definitions/json-legacy";
 
   @Bean
-  CCDConfig<CaseData, State, UserRole> jsonLegacyConfig(JsonBackedCCDConfigFactory jsonBackedCCDConfigFactory) {
-    return jsonBackedCCDConfigFactory.create(NoFaultDivorce.getCaseType(), JSON_ROOT);
+  CCDConfig<CaseData, State, UserRole> jsonLegacyConfig(ResourceLoader resourceLoader,
+                                                        ObjectMapper mapper,
+                                                        JsonCallbackAdapterFactory callbackAdapterFactory) {
+    return new JsonBackedCCDConfig<CaseData, State, UserRole>(
+        NoFaultDivorce.getCaseType(),
+        JSON_ROOT,
+        resourceLoader,
+        mapper,
+        callbackAdapterFactory
+    ) {};
   }
 }
