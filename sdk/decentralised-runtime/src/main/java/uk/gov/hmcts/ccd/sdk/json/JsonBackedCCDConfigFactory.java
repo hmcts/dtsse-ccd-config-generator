c8daed12ab9d7188f9fb641e8d986abc7bfaf756
Alex McAusland <a.m@user.com>
2026-06-05 10:17:44 +0100

    Use Lombok factory accessors

--- sdk/decentralised-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/json/JsonBackedCCDConfigFactory.java
+++ sdk/decentralised-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/json/JsonBackedCCDConfigFactory.java
@@ -1,28 +1,21 @@
 package uk.gov.hmcts.ccd.sdk.json;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.impl.json.JsonCallbackBridge;
 
 @Component
+@Getter(AccessLevel.PACKAGE)
 @RequiredArgsConstructor
+@Accessors(fluent = true)
 public class JsonBackedCCDConfigFactory {
 
   private final ResourceLoader resourceLoader;
   private final ObjectMapper mapper;
   private final JsonCallbackBridge callbackBridge;
-
-  ResourceLoader resourceLoader() {
-    return resourceLoader;
-  }
-
-  ObjectMapper mapper() {
-    return mapper;
-  }
-
-  JsonCallbackBridge callbackBridge() {
-    return callbackBridge;
-  }
 }
