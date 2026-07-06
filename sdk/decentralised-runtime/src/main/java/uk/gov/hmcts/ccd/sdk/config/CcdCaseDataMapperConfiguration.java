fea2f2853067e0380f0592bfeaf636f69f48022f
Alex McAusland <a.m@user.com>
2026-07-06 15:43:15 +0100

    Simplify case data mapper bean

--- sdk/decentralised-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/config/CcdCaseDataMapperConfiguration.java
+++ sdk/decentralised-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/config/CcdCaseDataMapperConfiguration.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.ccd.sdk.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
@@ -15,7 +14,6 @@ public class CcdCaseDataMapperConfiguration {
    * ObjectMapper used when serialising case data back to CCD's JSON wire shape.
    */
   @Bean(name = CCD_CASE_DATA_OBJECT_MAPPER)
-  @Qualifier(CCD_CASE_DATA_OBJECT_MAPPER)
   public ObjectMapper ccdCaseDataObjectMapper(ObjectMapper mapper) {
     return mapper.copy().setSerializationInclusion(JsonInclude.Include.NON_NULL);
   }
