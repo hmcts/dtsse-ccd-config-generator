088b04a4064f2f98a7bb9f9450c418a475f532d5
Alex McAusland <a.m@user.com>
2026-07-08 15:33:41 +0100

    Tidy legacy JSON CDAM test wiring

--- test-projects/e2e/src/main/java/uk/gov/hmcts/divorce/jsonlegacy/LegacyJsonDataModel.java
+++ test-projects/e2e/src/main/java/uk/gov/hmcts/divorce/jsonlegacy/LegacyJsonDataModel.java
@@ -1,12 +1,10 @@
 package uk.gov.hmcts.divorce.jsonlegacy;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 import java.util.Map;
 import lombok.Data;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class LegacyJsonDataModel {
 
   private String note;
