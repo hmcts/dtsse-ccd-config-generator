186732c4b9a0c5a53c8cf9e2b995ef7bc9c9370d
Alex McAusland <a.m@user.com>
2026-07-08 12:56:03 +0100

    Tidy significant item migration DDL

--- sdk/decentralised-runtime/src/main/resources/dataruntime-db/migration/V0022__case_event_significant_items.sql
+++ sdk/decentralised-runtime/src/main/resources/dataruntime-db/migration/V0022__case_event_significant_items.sql
@@ -1,18 +1,12 @@
 create type ccd.significant_item_type as enum ('DOCUMENT');
 
 create table ccd.case_event_significant_items (
-    id serial primary key,
-    description varchar(64) not null,
+    id bigserial primary key,
+    case_event_id bigint not null references ccd.case_event(id) on delete cascade,
     "type" ccd.significant_item_type not null,
-    url text,
-    case_event_id bigint not null
+    description varchar(64) not null,
+    url text
 );
-
-alter table ccd.case_event_significant_items
-    add constraint fk_case_event_items_case_event_id
-    foreign key (case_event_id)
-    references ccd.case_event(id)
-    on delete cascade;
 
 create index idx_case_event_significant_items_case_event_id
     on ccd.case_event_significant_items(case_event_id);
