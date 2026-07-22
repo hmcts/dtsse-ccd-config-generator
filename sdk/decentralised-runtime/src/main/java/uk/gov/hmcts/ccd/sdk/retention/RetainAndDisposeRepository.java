995c64c72d5a1fc8bcffb3819614bac691bc3229
Alex McAusland <a.m@user.com>
2026-07-22 14:13:12 +0100

    Inline retain and dispose SQL

--- sdk/decentralised-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/retention/RetainAndDisposeRepository.java
+++ sdk/decentralised-runtime/src/main/java/uk/gov/hmcts/ccd/sdk/retention/RetainAndDisposeRepository.java
@@ -11,45 +11,20 @@ import org.springframework.jdbc.core.simple.JdbcClient;
 @RequiredArgsConstructor
 class RetainAndDisposeRepository {
 
-  private static final String SELECT_CASES_BY_REFERENCE = """
-      select reference, jurisdiction, case_type_id
-      from ccd.case_data
-      where reference in (:caseReferences)
-        and case_type_id in (:caseTypeIds)
-        and resolved_ttl is null
-      order by reference asc
-      """;
-  private static final String SELECT_UNCONFIRMED_PENDING_DISPOSAL_CASES = """
-      select reference, jurisdiction, case_type_id
-      from ccd.case_data
-      where case_type_id in (:caseTypeIds)
-        and state = :state
-        and resolved_ttl is null
-      order by reference asc
-      """;
-  private static final String SELECT_EXPIRED_PENDING_DISPOSAL_CASES = """
-      select reference, jurisdiction, case_type_id
-      from ccd.case_data
-      where case_type_id in (:caseTypeIds)
-        and state = :state
-        and resolved_ttl < current_date
-      order by reference asc
-      """;
-  private static final String DELETE_PENDING_DISPOSAL_CASE = """
-      delete from ccd.case_data
-      where reference = :reference
-        and case_type_id = :caseTypeId
-        and state = :state
-        and resolved_ttl < current_date
-      """;
-
   private final JdbcClient db;
 
   List<RetainAndDisposeCase> resolveCandidates(Collection<Long> caseReferences, Set<String> caseTypeIds) {
     if (caseReferences.isEmpty()) {
       return List.of();
     }
-    return db.sql(SELECT_CASES_BY_REFERENCE)
+    return db.sql("""
+            select reference, jurisdiction, case_type_id
+            from ccd.case_data
+            where reference in (:caseReferences)
+              and case_type_id in (:caseTypeIds)
+              and resolved_ttl is null
+            order by reference asc
+            """)
         .param("caseReferences", caseReferences)
         .param("caseTypeIds", caseTypeIds)
         .query(RetainAndDisposeCase.class)
@@ -57,7 +32,14 @@ class RetainAndDisposeRepository {
   }
 
   List<RetainAndDisposeCase> findUnconfirmedPendingDisposalCases(Set<String> caseTypeIds) {
-    return db.sql(SELECT_UNCONFIRMED_PENDING_DISPOSAL_CASES)
+    return db.sql("""
+            select reference, jurisdiction, case_type_id
+            from ccd.case_data
+            where case_type_id in (:caseTypeIds)
+              and state = :state
+              and resolved_ttl is null
+            order by reference asc
+            """)
         .param("caseTypeIds", caseTypeIds)
         .param("state", DISPOSAL_STATE_ID)
         .query(RetainAndDisposeCase.class)
@@ -65,7 +47,14 @@ class RetainAndDisposeRepository {
   }
 
   List<RetainAndDisposeCase> findExpiredPendingDisposalCases(Set<String> caseTypeIds) {
-    return db.sql(SELECT_EXPIRED_PENDING_DISPOSAL_CASES)
+    return db.sql("""
+            select reference, jurisdiction, case_type_id
+            from ccd.case_data
+            where case_type_id in (:caseTypeIds)
+              and state = :state
+              and resolved_ttl < current_date
+            order by reference asc
+            """)
         .param("caseTypeIds", caseTypeIds)
         .param("state", DISPOSAL_STATE_ID)
         .query(RetainAndDisposeCase.class)
@@ -73,7 +62,13 @@ class RetainAndDisposeRepository {
   }
 
   void deletePendingDisposalCase(RetainAndDisposeCase disposalCase) {
-    int deleted = db.sql(DELETE_PENDING_DISPOSAL_CASE)
+    int deleted = db.sql("""
+            delete from ccd.case_data
+            where reference = :reference
+              and case_type_id = :caseTypeId
+              and state = :state
+              and resolved_ttl < current_date
+            """)
         .param("reference", disposalCase.reference())
         .param("caseTypeId", disposalCase.caseTypeId())
         .param("state", DISPOSAL_STATE_ID)
