4408b45ef4254494d4c797d9d49fadd64396b4ac
Alex McAusland <a.m@user.com>
2026-08-29 14:31:52 +0100

    protect clauses from splitting on enter

--- frontend/order-flow/src/keymap.ts
+++ frontend/order-flow/src/keymap.ts
@@ -1,16 +1,53 @@
 import {
-  baseKeymap, chainCommands,
+  baseKeymap,
+  chainCommands,
   toggleMark,
 } from "prosemirror-commands";
 import { redo, undo } from "prosemirror-history";
 import { keymap } from "prosemirror-keymap";
 import { splitListItem } from "prosemirror-schema-list";
-import { type Command, type Plugin } from "prosemirror-state";
+import {
+  type Command,
+  type Plugin,
+  TextSelection,
+} from "prosemirror-state";
 
 import { editorSchema } from "./schema.js";
 
 const mac = typeof navigator !== "undefined" &&
   /Mac|iP(hone|[oa]d)/.test(navigator.platform);
+
+const protectClausesFromSplittingOnEnter: Command = (state, dispatch) => {
+  const { $cursor } = state.selection as TextSelection;
+  if (!$cursor) return false;
+
+  const paragraphType = editorSchema.nodes.paragraph!;
+  const listItemType = editorSchema.nodes.list_item!;
+  const parentNode = $cursor.node(-1);
+  const isListItem = parentNode.type === listItemType;
+  const clauseNode = isListItem ? parentNode : $cursor.parent;
+
+  if (typeof clauseNode.attrs.clause_id !== "string") return false;
+
+  if (!dispatch) return true;
+
+  const clauseDepth = isListItem ? $cursor.depth - 1 : $cursor.depth;
+  const insertPosition = $cursor.after(clauseDepth);
+  const node = isListItem
+    ? listItemType.create(null, paragraphType.create())
+    : paragraphType.create();
+  const transaction = state.tr.insert(insertPosition, node);
+
+  transaction.setSelection(
+    TextSelection.create(
+      transaction.doc,
+      insertPosition + (isListItem ? 2 : 1),
+    ),
+  );
+
+  dispatch(transaction.scrollIntoView());
+  return true;
+};
 
 function buildKeymap(): Record<string, Command> {
   const bindings: Record<string, Command> = {};
@@ -32,12 +69,13 @@ function buildKeymap(): Record<string, Command> {
     editorSchema.nodes.list_item!,
   );
 
-  bind("Enter",
+  bind(
+    "Enter",
     chainCommands(
-      // insertUserContentAfterIdentifiedClause,
+      protectClausesFromSplittingOnEnter,
       splitListItemOnEnter,
     ),
-    );
+  );
 
   return bindings;
 }
